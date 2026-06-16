85112cb1786d6a2726f1df8d6f093e31e9168a70
Miltiadis-Kon <miltiadiskontos@gmail.com>
2026-06-16 16:19:16 +0300

    upd nix

--- replit.nix
+++ replit.nix
@@ -2,5 +2,27 @@
   deps = [
     pkgs.python3
     pkgs.sqlite
+    pkgs.nss
+    pkgs.nspr
+    pkgs.atk
+    pkgs.at-spi2-atk
+    pkgs.cups
+    pkgs.libdrm
+    pkgs.expat
+    pkgs.libxcb
+    pkgs.libxkbcommon
+    pkgs.xorg.libXcomposite
+    pkgs.xorg.libXdamage
+    pkgs.xorg.libXfixes
+    pkgs.xorg.libXrandr
+    pkgs.xorg.libXext
+    pkgs.xorg.libX11
+    pkgs.libgbm
+    pkgs.pango
+    pkgs.cairo
+    pkgs.alsa-lib
+    pkgs.dbus
+    pkgs.glib
+    pkgs.gtk3
   ];
 }
